a51a297daff29adbec626fa1de602f782422dce2
sanarajan <sanamol87@gmail.com>
2025-12-23 12:07:27 +0530

    create env

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+import { API_BASE_URL } from '../common/constants';
+
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: API_BASE_URL,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
